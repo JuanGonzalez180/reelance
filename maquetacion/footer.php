683b6802359ebe02f1c72e465427d2eb62bd9010
JuanGonzalez180 <juangon.t@gmail.com>
2019-09-14 00:07:08 -0500

    Footer

--- maquetacion/footer.php
+++ maquetacion/footer.php
@@ -1,3 +1,90 @@
+            <footer class="page-footer footer-luma">
+
+                <!-- footer content -->
+                <div class="footer">
+                    <div class="footer-bootom">
+                        <div class="container">
+
+                            <div class="row">						    	
+                                <div class="col-sm-3">
+                                    <div class="col-md-2-width">
+                                        <h3 class="about">NOSOTROS</h3>
+                                        <ul class="footer links">
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Nuestros Objetivos</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Nuestra Misión</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Nuestra Visión</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-3">
+                                    <div class="col-md-2-width">
+                                        <h3 class="about">INFO MINOXIDIL</h3>
+                                        <ul class="footer links">
+                                            <li><a href="#"><i class="fa fa-caret-right "></i>Minoxidil </a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right "></i>Minoxidil 5 </a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right "></i>Minoxidil México </a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right "></i>Minoxidil Precio </a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right "></i>Minoxidil Shampoo </a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-3">
+                                    <div class="col-md-2-width ">
+                                        <h3 class="about">INFO ALOPECIA</h3>
+                                        <ul class="footer links">
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Alopecia</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Alopecia en Mujeres</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>iAlopecia tratamiento</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Caida de Cabello</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Perdida de Cabello</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-3">
+                                    <div class="col-md-2-width col-footer">
+                                        <h3 class="about about-right">INFO TRATAMIENTOS</h3>
+                                        <ul class="footer links">
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Crecimiento del cabello</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Calvicie en mujeres</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Tratamiento para la Alopecia</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Tratamiento para la calvicie</a></li>
+                                            <li><a href="#"><i class="fa fa-caret-right"></i>Tratamiento para la caída del cabello</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- end footer content -->
+
+                    <!--  copyright -->
+                    <div class="copyright">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-6">
+                                    <p class="a-center">Copyright © 2016 <span class="bluer"><a href="">EngoTheme</a></span>. All Right Reserved</p>
+
+                                </div>
+                                <div class="col-sm-6">
+                                    <ul>
+                                        <li><a href="#"><img src="images/assets/link-1.png" alt=""></a></li>
+                                        <li><a href="#"><img src="images/assets/link-2.png" alt=""></a></li>
+                                        <li><a href="#"><img src="images/assets/link-3.png" alt=""></a></li>
+                                        <li><a href="#"><img src="images/assets/link-4.png" alt=""></a></li>
+                                        <li><a href="#"><img src="images/assets/link5.png" alt=""></a></li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end copyright -->	
+                </div>	
+            </footer>
         </div>
     </body>
 </html>
