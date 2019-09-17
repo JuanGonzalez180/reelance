f7ebb56f2da158ddf0eac413c5969514629623cc
JuanGonzalez180 <juangon.t@gmail.com>
2019-09-16 23:49:25 -0500

    Promociones Home

--- maquetacion/inicio.php
+++ maquetacion/inicio.php
@@ -74,33 +74,103 @@
                 </div>
             </div>
         </div>
-        <!-- Add Pagination -->
-        <div class="swiper-pagination swiper-testi-pagination"></div>
     </div>
 </section>
 
-<section class="container container-padd-ree">
-    <ul class="row ree-container-nosotros">
-        <li class="col-md-4 ree-item-nosotros">
-            <i class="lni-users"></i>
-            <h3>QUIENES SOMOS</h3>
-            <p>Somos una empresa con más de 7 años de experiencia en el estudio cercano de la alopecia y la caída del cabello en general. Nos diferenciamos de otras empresas principalmente porque compartimos todos los datos obtenidos a través de nuestros estudios para que te mantengas informado y conozcas al detalle lo que guarda estrecha relación con la alopecia.</p>
-            <div class="separator-btn"></div>
-            <a href="#" class="btn-page txt-upper">Leer más</a>
-        </li>
-        <li class="col-md-4 ree-item-nosotros">
-            <i class="lni-star"></i>
-            <h3>QUÉ HACEMOS</h3>
-            <p>Nuestra misión principal se basa en la investigación constante de la alopecia y en nuestro esfuerzo diario por mantener informados a los usuarios, que de una u otra manera acuden a Internet para entender el por qué de la pérdida del cabello, diferenciar la alopecia de la caída normal, entre otras dudas más.</p>
-            <div class="separator-btn"></div>
-            <a href="#" class="btn-page txt-upper">Leer más</a>
-        </li>
-        <li class="col-md-4 ree-item-nosotros">
-            <i class="lni-question-circle"></i>
-            <h3>POR QUÉ NOSOTROS</h3>
-            <p>Al iniciar la lectura de “Por qué Reelance y no otro” seguro pensarás: A ver que tienen estos por contar. Y en realidad nosotros, así como la competencia, también estamos interesados en vender nuestros productos, pero trabajamos por cada uno de ustedes un poco más.</p>
-            <div class="separator-btn"></div>
-            <a href="#" class="btn-page txt-upper">Leer más</a>
-        </li>
-    </ul>
+<section class="box-bottom">
+    <div class="container container-padd-ree">
+        <ul class="row ree-container-nosotros">
+            <li class="col-md-4 ree-item-nosotros">
+                <i class="lni-users"></i>
+                <h3>QUIENES SOMOS</h3>
+                <p>Somos una empresa con más de 7 años de experiencia en el estudio cercano de la alopecia y la caída del cabello en general. Nos diferenciamos de otras empresas principalmente porque compartimos todos los datos obtenidos a través de nuestros estudios para que te mantengas informado y conozcas al detalle lo que guarda estrecha relación con la alopecia.</p>
+                <div class="separator-btn"></div>
+                <a href="#" class="btn-page txt-upper">Leer más</a>
+            </li>
+            <li class="col-md-4 ree-item-nosotros">
+                <i class="lni-star"></i>
+                <h3>QUÉ HACEMOS</h3>
+                <p>Nuestra misión principal se basa en la investigación constante de la alopecia y en nuestro esfuerzo diario por mantener informados a los usuarios, que de una u otra manera acuden a Internet para entender el por qué de la pérdida del cabello, diferenciar la alopecia de la caída normal, entre otras dudas más.</p>
+                <div class="separator-btn"></div>
+                <a href="#" class="btn-page txt-upper">Leer más</a>
+            </li>
+            <li class="col-md-4 ree-item-nosotros">
+                <i class="lni-question-circle"></i>
+                <h3>POR QUÉ NOSOTROS</h3>
+                <p>Al iniciar la lectura de “Por qué Reelance y no otro” seguro pensarás: A ver que tienen estos por contar. Y en realidad nosotros, así como la competencia, también estamos interesados en vender nuestros productos, pero trabajamos por cada uno de ustedes un poco más.</p>
+                <div class="separator-btn"></div>
+                <a href="#" class="btn-page txt-upper">Leer más</a>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<section style="background: #f8f8f8;"> 
+    <div class="container container-padd-ree">
+        <h2 class="tit-ree">Promociones Recomendadas</h2>
+
+        <div class="swiper-container swiper-container-promociones">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="swiper-promo">
+                        <div class="img-promo">
+                            <img src="images/assets/producto-1.png" />
+                            <div class="price-promo">$1.000,00</div>
+                        </div>
+                        <h3>Tratamiento Nutritivo Restaurador + Reelance Mujer</h3>
+                        <div class="separator-line"></div>
+                        <a href="#" class="btn-page txt-upper">Añadir al carrito</a>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="swiper-promo">
+                        <div class="img-promo">
+                            <img src="images/assets/producto-2.png" />
+                            <div class="price-promo">$1.000,00</div>
+                        </div>
+                        <h3>Dos Tratamiento Nutritivo Restaurador</h3>
+                        <div class="separator-line"></div>
+                        <a href="#" class="btn-page txt-upper">Añadir al carrito</a>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="swiper-promo">
+                        <div class="img-promo">
+                            <img src="images/assets/producto-3.png" />
+                            <div class="price-promo">$1.000,00</div>
+                        </div>
+                        <h3>Potenciador para Crecimiento de Pestañas</h3>
+                        <div class="separator-line"></div>
+                        <a href="#" class="btn-page txt-upper">Añadir al carrito</a>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="swiper-promo">
+                        <div class="img-promo">
+                            <img src="images/assets/producto-1.png" />
+                            <div class="price-promo">$1.000,00</div>
+                        </div>
+                        <h3>Alopecia Congenia</h3>
+                        <div class="separator-line"></div>
+                        <a href="#" class="btn-page txt-upper">Añadir al carrito</a>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="swiper-promo">
+                        <div class="img-promo">
+                            <img src="images/assets/producto-2.png" />
+                            <div class="price-promo">$1.000,00</div>
+                        </div>
+                        <h3>Alopecia Congenia</h3>
+                        <div class="separator-line"></div>
+                        <a href="#" class="btn-page txt-upper">Añadir al carrito</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Add Arrows -->
+            <div class="swiper-button-next swiper-btn-promo-next"></div>
+            <div class="swiper-button-prev swiper-btn-promo-prev"></div>
+        </div>
+
+    </div>
 </section>
--- maquetacion/js/custom.js
+++ maquetacion/js/custom.js
@@ -5,10 +5,20 @@ var swiperBanner = new Swiper('.swiper-container-banner', {
     },
     pagination: {
         el: '.swiper-ban-pagination',
+        clickable: true,
     },
 });
 
 var swiperTesti = new Swiper('.swiper-container-testi', {
     slidesPerView: 4,
     spaceBetween: 30
+});
+
+var swiperPromo = new Swiper('.swiper-container-promociones', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-btn-promo-next',
+        prevEl: '.swiper-btn-promo-prev',
+    },
 });
