86fa19a9b2c3fc446db37f91b20499492a90931d
omarfandino <omar.fandino@incdustry.com>
2019-09-18 00:05:21 -0500

    Tabla de precios

--- maquetacion/tienda.php
+++ maquetacion/tienda.php
@@ -16,41 +16,105 @@
         <div class="swiper-wrapper">
             <div class="swiper-slide">
                 <div class="swiper-products">
-                    <h3>Pedro Buendia</h3>
-                    <img src="images/assets/testimonio-1.png" />
-                    <p>Alopecia Congenia</p>
+                    <img src="images/assets/producto-1.png" />
+                    <h3>Crecimiento cabello</h3>
+                    <p>Hombre</p>
                 </div>
             </div>
             <div class="swiper-slide">
                 <div class="swiper-products">
-                    <h3>Cesar Millan</h3>
-                    <img src="images/assets/testimonio-1.png" />
-                    <p>Crecimiento Barba</p>
+                    <img src="images/assets/producto-2.png" />
+                    <h3>Crecimiento cabello</h3>
+                    <p>Mujer</p>
                 </div>
             </div>
             <div class="swiper-slide">
                 <div class="swiper-products">
-                    <h3>Gerardo Goméz</h3>
-                    <img src="images/assets/testimonio-1.png" />
-                    <p>Alopecia Congenia</p>
+                    <img src="images/assets/producto-3.png" />
+                    <h3>Crecimiento de cejas</h3>
+                    <p>Hombres y Mujeres</p>
                 </div>
             </div>
             <div class="swiper-slide">
                 <div class="swiper-products">
-                    <h3>Pedro Buendia</h3>
-                    <img src="images/assets/testimonio-1.png" />
-                    <p>Alopecia Congenia</p>
+                    <img src="images/assets/producto-2.png" />
+                    <h3>Crecimiento cabello</h3>
+                    <p>Mujer</p>
                 </div>
             </div>
             <div class="swiper-slide">
                 <div class="swiper-products">
-                    <h3>Pedro Buendia</h3>
-                    <img src="images/assets/testimonio-1.png" />
-                    <p>Alopecia Congenia</p>
+                    <img src="images/assets/producto-1.png" />
+                    <h3>Crecimiento cabello</h3>
+                    <p>Hombre</p>
                 </div>
             </div>
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination swiper-products-pagination"></div>
+    </div>
+</section>
+<section class="container container-padd-ree">
+    <h2 class="tit-ree">Promociones</h2>
+    <div class="pricing-table">
+        <div class="col-xs-3 pricing-column">
+            <h3>1 MES DE TRATAMIENTO</h3>
+            <div class="pricing-column-content">
+                <h4>
+                    <span class="dollar-sign">$</span> 600
+                </h4>
+                <span class="interval">Envío gratis</span>
+                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/reelance_homre_1-300x221.png" alt="" width="300" height="221">
+                <a href="#" class="btn-price btn-page txt-upper">Comprar</a>
+            </div>
+        </div><div class="col-xs-3 pricing-column highlight">
+            <h3>
+                3 MESES DE TRATAMIENTO
+                <span class="highlight-reason">Recomendado</span>
+            </h3>
+            <div class="pricing-column-content">
+                <h4>
+                    <span class="dollar-sign">$</span> 600
+                </h4>
+                <span class="interval">Envío gratis</span>
+                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/reelance_homre_4-300x221.png" alt="" width="300" height="221">
+                <a href="#" class="btn-price btn-page txt-upper">Comprar</a>
+            </div>
+        </div><div class="col-xs-3 pricing-column">
+            <h3>
+                2 MESES DE TRATAMIENTO
+            </h3>
+            <div class="pricing-column-content">
+                <h4>
+                    <span class="dollar-sign">$</span> 600
+                </h4>
+                <span class="interval">Envío gratis</span>
+                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/reelance_homre_2-300x221.png" alt="" width="300" height="221">
+                <a href="#" class="btn-price btn-page txt-upper">Comprar</a>
+            </div>
+        </div><div class="col-xs-3 pricing-column pricing-custom">
+            <h3>
+                <input type="number" class="qty" step="1" min="1" max="" name="quantity" value="1" title="Cantidad" size="4" inputmode="numeric"> MESES DE TRATAMIENTO
+            </h3>
+            <div class="pricing-column-content">
+                <h4>
+                    <span class="dollar-sign">$</span> 600
+                </h4>
+                <span class="interval">Envío gratis</span>
+                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/reelance_homre_1-300x221.png" alt="" width="300" height="221">
+                <a href="#" class="btn-price btn-page txt-upper">Comprar</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content-grid banner-fedex">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-3 fedex-img">
+                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/fedexlogoblanco2.png" alt="">
+            </div><div class="col-xs-9 fedex-img">
+                <h3>ENVÍOS EXPRESS GRATIS A TODOS LOS ESTADOS DE MÉXICO. TIEMPO ESTIMADO DE ENTREGA DE 1 A 4 DÍAS.</h3>
+            </div>
+        </div>
     </div>
 </section>
