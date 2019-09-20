ce5e05a4f1d532737a8bc8538f41c1063624b0c0
omarfandino <omar.fandino@incdustry.com>
2019-09-20 00:14:11 -0500

    Mapa contacto

--- maquetacion/contacto.php
+++ maquetacion/contacto.php
@@ -1,120 +1,47 @@
 <section class="content-grid">
-	<div class="images-banner">
-		<img src="images/assets/images-titulo-ree.jpg" alt="">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12">
-					<h1 class="images-titulo-ree txt-upper">Enmarca tu mirada</h1>
-				</div>
-			</div>
-		</div>
-	</div>
+	<div id="gmaps-ree" class="gmaps-ree"></div>
 </section>
-<section class="container container-padd-ree">
-    <h2 class="tit-ree">¿Que necesitas?</h2>
-    <div class="swiper-container swiper-container-products">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <div class="swiper-products">
-                    <img src="images/assets/producto-1.png" />
-                    <h3>Crecimiento cabello</h3>
-                    <p>Hombre</p>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="swiper-products">
-                    <img src="images/assets/producto-2.png" />
-                    <h3>Crecimiento cabello</h3>
-                    <p>Mujer</p>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="swiper-products">
-                    <img src="images/assets/producto-3.png" />
-                    <h3>Crecimiento de cejas</h3>
-                    <p>Hombres y Mujeres</p>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="swiper-products">
-                    <img src="images/assets/producto-2.png" />
-                    <h3>Crecimiento cabello</h3>
-                    <p>Mujer</p>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="swiper-products">
-                    <img src="images/assets/producto-1.png" />
-                    <h3>Crecimiento cabello</h3>
-                    <p>Hombre</p>
-                </div>
-            </div>
-        </div>
-        <!-- Add Pagination -->
-        <div class="swiper-pagination swiper-products-pagination"></div>
-    </div>
-</section>
-<section class="container container-padd-ree">
-    <h2 class="tit-ree">Promociones</h2>
-    <div class="pricing-table">
-        <div class="col-xs-3 pricing-column">
-            <h3>1 MES DE TRATAMIENTO</h3>
-            <div class="pricing-column-content">
-                <h4>
-                    <span class="dollar-sign">$</span> 600
-                </h4>
-                <span class="interval">Envío gratis</span>
-                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/reelance_homre_1-300x221.png" alt="" width="300" height="221">
-                <a href="#" class="btn-price btn-page txt-upper">Comprar</a>
-            </div>
-        </div><div class="col-xs-3 pricing-column highlight">
-            <h3>
-                3 MESES DE TRATAMIENTO
-                <span class="highlight-reason">Recomendado</span>
-            </h3>
-            <div class="pricing-column-content">
-                <h4>
-                    <span class="dollar-sign">$</span> 600
-                </h4>
-                <span class="interval">Envío gratis</span>
-                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/reelance_homre_4-300x221.png" alt="" width="300" height="221">
-                <a href="#" class="btn-price btn-page txt-upper">Comprar</a>
-            </div>
-        </div><div class="col-xs-3 pricing-column">
-            <h3>
-                2 MESES DE TRATAMIENTO
-            </h3>
-            <div class="pricing-column-content">
-                <h4>
-                    <span class="dollar-sign">$</span> 600
-                </h4>
-                <span class="interval">Envío gratis</span>
-                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/reelance_homre_2-300x221.png" alt="" width="300" height="221">
-                <a href="#" class="btn-price btn-page txt-upper">Comprar</a>
-            </div>
-        </div><div class="col-xs-3 pricing-column pricing-custom">
-            <h3>
-                <input type="number" class="qty" step="1" min="1" max="" name="quantity" value="1" title="Cantidad" size="4" inputmode="numeric"> MESES DE TRATAMIENTO
-            </h3>
-            <div class="pricing-column-content">
-                <h4>
-                    <span class="dollar-sign">$</span> 600
-                </h4>
-                <span class="interval">Envío gratis</span>
-                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/reelance_homre_1-300x221.png" alt="" width="300" height="221">
-                <a href="#" class="btn-price btn-page txt-upper">Comprar</a>
-            </div>
-        </div>
-    </div>
-</section>
-<section class="content-grid banner-fedex">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-3 dp-ib fedex-img">
-                <img src="https://www.reelance.mx/wp-content/uploads/2015/04/fedexlogoblanco2.png" alt="">
-            </div><div class="col-xs-9 dp-ib fedex-img">
-                <h3>ENVÍOS EXPRESS GRATIS A TODOS LOS ESTADOS DE MÉXICO. TIEMPO ESTIMADO DE ENTREGA DE 1 A 4 DÍAS.</h3>
-            </div>
-        </div>
-    </div>
-</section>
+<script>
+      // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('gmaps-ree'), {
+          center: {lat: 4.662390, lng: -74.109183},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            var marker = new google.maps.Marker({position: pos, map: map});
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Ubicación encontrada.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: El servicio de geolocalización ha fallado.' :
+                              'Error: Tu navegador no admite geolocalización.');
+        infoWindow.open(map);
+      }
+</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?callback=initMap"></script>
