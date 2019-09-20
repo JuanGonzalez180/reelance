ca17d4ee4b233c7a00e66c140031abd278baf33b
omarfandino <omar.fandino@incdustry.com>
2019-09-20 13:11:34 -0500

    Home Actualizado

--- maquetacion/inicio.php
+++ maquetacion/inicio.php
@@ -308,3 +308,22 @@
         </div>
     </section>
 </div>
+
+<section class="content-grid">
+    <div class="ree-redes-sociales">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <h4>Encuéntranos en Redes Sociales</h4>
+                    <ul>
+                        <li><a href=""><i class="lni-facebook-filled"></i></a></li>
+                        <li><a href=""><i class="lni-instagram-original"></i></a></li>
+                        <li><a href=""><i class="lni-twitter-original"></i></a></li>
+                        <li><a href=""><i class="lni-pinterest"></i></a></li>
+                        <li><a href=""><i class="lni-youtube"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
